b33635830b6c07006f02b388e07c94acc944b089
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-03-30 18:00:56 +0500

    Practice till Sync or Async

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 
 // function foo() {
 //     console.log("Hello World");
-    
+
 // }
 
 
@@ -45,10 +45,10 @@
 // var para = document.children[0].children[1].style.color = "orange"
 
 var std = {
-    Name : "Ghulam Mustafa",
-    age : 16,
-    city : "Karachi",
-    email :  "mustafa@gmail.com",
+    Name: "Ghulam Mustafa",
+    age: 16,
+    city: "Karachi",
+    email: "mustafa@gmail.com",
 
 }
 
@@ -63,15 +63,15 @@ var std = {
 
 var stds = [
     {
-        name : "Mustafa",
-        fatherName : "Shahzad",
-        age : 16
-    } ,
-     {
-        name : "Shafay",
-        fatherName : "Shahzad",
-        age : 17
-    } 
+        name: "Mustafa",
+        fatherName: "Shahzad",
+        age: 16
+    },
+    {
+        name: "Shafay",
+        fatherName: "Shahzad",
+        age: 17
+    }
 ]
 
 // console.log(stds[0]["name"]);
@@ -84,44 +84,186 @@ var stds = [
 
 // }
 
-var parent = document.getElementById("parent")
+// var parent = document.getElementById("parent")
 
-fetch("https://fakestoreapi.com/products")
-.then(
-    function (response) {
-console.log(response);
-return response.json()
-    }
-)
+// fetch("https://fakestoreapi.com/products")
+// .then(
+//     function (response) {
+// console.log(response);
+// return response.json()
+//     }
+// )
 
-.then(
-    function (success) {
-console.log(success);
-
-
-for(var i = 0 ; i < success.length ; i++){
-console.log(i);
-var card = `<div class="card" style="width: 18rem;">
-  <img src=${success[i].image}class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${success[i].title}</h5>
-    <p class="card-text">${success[i].description}</p>
-    <a href="#" class="btn btn-primary">${success[i].price}$</a>
-  </div>
-</div>`
-
-parent.innerHTML += card
-
-}
+// .then(
+//     function (success) {
+// console.log(success);
 
 
+// for(var i = 0 ; i < success.length ; i++){
+// console.log(i);
+// var card = `<div class="card" style="width: 18rem;">
+//   <img src=${success[i].image}class="card-img-top" alt="...">
+//   <div class="card-body">
+//     <h5 class="card-title">${success[i].title}</h5>
+//     <p class="card-text">${success[i].description}</p>
+//     <a href="#" class="btn btn-primary">${success[i].price}$</a>
+//   </div>
+// </div>`
 
-    }
-)
+// parent.innerHTML += card
 
-.catch(
-    function (error) {
-console.log(error);
+// }
 
-    }
-)
+
+
+//     }
+// )
+
+// .catch(
+//     function (error) {
+// console.log(error);
+
+//     }
+// )
+
+
+
+// var correctAns = +prompt("Enter how many correct ans you give")
+// var percentage;
+// switch (correctAns) {
+//     case 10:
+//         percentage = (10 * 100) + "%"
+//         break
+//     case 9: percentage = "90%"
+//         break
+//     case 8: percentage = "80%"
+//         break
+//     case 7: percentage = "70%"
+//         break
+//     case 6: percentage = "60%"
+//         break
+//     case 5: percentage = "50%"
+//         break
+//     case 4: percentage = "40%"
+//         break
+//     case 3: percentage = "30%"
+//         break
+//     case 2: percentage = "20%"
+//         break
+//     case 1: percentage = "10%"
+//         break
+// }
+
+
+
+
+// percentage = (correctAns * 10) + "%"
+
+// alert(percentage)
+
+
+
+
+// console.log("--- For Loop ---");
+
+
+
+// for (var j = 1; j <= 10; j++) {
+//     console.log(j);
+
+// }
+
+
+// console.log("--- While Loop ---");
+
+
+// var e = 1;
+
+// while (e <= 10) {
+
+//     console.log(e);
+
+//     e++
+// }
+
+// console.log("--- Do-While Loop ---");
+
+
+// var i = 1
+// do {
+//     console.log(i);
+
+
+//     i++
+// } while (i <= 10)
+
+
+
+// console.log("---Alag---");
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+
+
+
+// console.log("---Array---");
+
+// var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// for(data of num){
+// console.log(data);
+// }
+
+
+// console.log("---new nethod ---");
+// var arr = []
+
+// for(var i = 1 ; i <= 1000 ; i++){
+// arr.push(i)
+
+// }
+// console.log(arr);
+
+
+
+
+// for(val of arr){
+// console.log(val);
+
+// }
+
+
+// var i = 1
+
+// do{
+// console.log(i);
+// i++
+// }while( i <= 10)
+
+
+
+
+
+
+// console.log("Start");
+// setInterval(
+//     function () {
+// for(var i = 1  ; i <= 10 ; i++){
+// console.log(i);
+// if(i == 10){
+// console.log("---End---");
+
+// }
+// }
+
+
+//     } , 1000
+// )
+// console.log("End");
